da55b2360daf5156610a2384649490207cb0021f
Alex Sausedos <maeroff93@yandex.by>
2022-02-19 23:50:12 +0300

    test2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, to, World!");
+﻿// Запросим ввод строки
+Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
